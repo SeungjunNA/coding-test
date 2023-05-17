26e8540a4606cf7c38761b17c661d703cea2ddf5
SeungjunNA <nsjjsng1@gmail.com>
2023-05-18 01:50:16 +0900

    코딩테스트 대비 문제 풀이

--- src/baekjoon/_11659.java
+++ src/baekjoon/_11659.java
@@ -1,10 +1,29 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class _11659 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		int M = scan.nextInt();
+		
+		int[] arr = new int[N+1];
+		int[] sum = new int[N+1];
+		int[] result = new int[M]; 
+		for(int i=1;i<N+1;i++) {
+			arr[i] = scan.nextInt();
+			sum[i] = sum[i-1] + arr[i];
+		}
+		for(int k=0;k<M;k++) {
+			int i = scan.nextInt();
+			int j = scan.nextInt();
+			result[k] = sum[j]-sum[i-1];	
+			System.out.println(result[k]);
+		}
+		
 	}
 
 }
