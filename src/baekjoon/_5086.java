e0e7d9c01444b866e3e2303dbb562d74e7c894b2
SeungjunNA <nsjjsng1@gmail.com>
2023-05-25 14:29:57 +0900

    코딩테스트 대비 문제 풀이

---
+++ src/baekjoon/_5086.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class _5086 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		while(true) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			if(a==0 && b==0) break;
+			else if(a%b==0) System.out.println("multiple");
+			else if(b%a==0) System.out.println("factor");
+			else System.out.println("neither");
+		}
+	}
+
+}
