db41a48e8f35c28a3733370da16cc054970e3ac7
SeungjunNA <nsjjsng1@gmail.com>
2023-06-12 23:55:01 +0900

    코딩테스트 대비 문제 풀이

---
+++ src/baekjoon/_10773.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class _10773 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		int sum = 0;
+		
+		for(int i=0;i<n;i++) {
+			int num = Integer.parseInt(br.readLine());
+			if(num==0) {
+				stack.pop();
+			}
+			else stack.push(num);
+		}
+		while(!stack.empty())
+			sum += stack.pop();
+		System.out.println(sum);
+	}
+
+}
---
+++ src/baekjoon/_2475.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class _2475 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int sum = 0;
+		for(int i=0;i<5;i++)
+    		sum += Math.pow(Integer.parseInt(st.nextToken()), 2);
+		System.out.println(sum%10);
+		
+    }
+}
