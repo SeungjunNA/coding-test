e287ec7951f94644a99628782888bfa86ea298a1
SeungjunNA <nsjjsng1@gmail.com>
2023-08-01 00:17:56 +0900

    코딩테스트 대비 문제 풀이

--- src/baekjoon/_11729.java
+++ src/baekjoon/_11729.java
@@ -2,7 +2,7 @@ package baekjoon;
 
 import java.util.*;
 import java.io.*;
-public class MyClass {
+public class _11729 {
     static StringBuilder sb = new StringBuilder();
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
