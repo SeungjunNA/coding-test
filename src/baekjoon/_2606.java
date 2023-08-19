27aeffa9706d2a31d90fd163d74f48f2072a1709
SeungjunNA <nsjjsng1@gmail.com>
2023-08-19 23:20:01 +0900

    코딩테스트 대비 문제 풀이

--- src/baekjoon/_2606.java
+++ src/baekjoon/_2606.java
@@ -38,3 +38,46 @@ public class _2606 {
         }
     }
 }
+//bfs¹æ½Ä
+//public class _2606 {
+//    static int[][] network;
+//    static boolean[] visit;
+//    static int count = 0;
+//    static int n;
+//    static int k;
+//    public static void main(String args[]) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        n = Integer.parseInt(br.readLine());
+//        k = Integer.parseInt(br.readLine());
+//        
+//        network = new int[n+1][n+1];
+//        visit = new boolean[n+1];
+//
+//        for(int i=0;i<k;i++){
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            network[a][b] = network[b][a] = 1;
+//        }
+//        
+//        bfs();
+//        
+//        System.out.println(count);
+//    }
+//    static void bfs(){
+//        LinkedList<Integer> queue = new LinkedList<>();
+//        queue.add(1);
+//        visit[1] = true;
+//        
+//        while(!queue.isEmpty()){
+//            int m = queue.poll();
+//            for(int i=1;i<=n;i++){
+//                if(network[m][i]==1 && !visit[i]){
+//                    visit[i] = true;
+//                    queue.add(i);
+//                    count++;
+//                }
+//            }
+//        }
+//    }
+//}
--- src/baekjoon/_2667.java
+++ src/baekjoon/_2667.java
@@ -2,6 +2,7 @@ package baekjoon;
 
 import java.util.*;
 import java.io.*;
+//dfs
 public class _2667 {
     static int n;
     static int[][] matrix;
@@ -53,3 +54,72 @@ public class _2667 {
         }
     }
 }
+//bfs
+//public class _2667 {
+//    static int[][] matrix;
+//    static boolean[][] visit;
+//    static int n;
+//    static LinkedList<Matrix> queue = new LinkedList<>();
+//    static List<Integer> list = new ArrayList<>();
+//    static int count;
+//    static int[] x_move = {1,0,-1,0};
+//    static int[] y_move = {0,1,0,-1};
+//    public static void main(String args[]) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        n = Integer.parseInt(br.readLine());
+//        
+//        matrix = new int[n][n];
+//        visit = new boolean[n][n];
+//        
+//        for(int i=0;i<n;i++){
+//            String str = br.readLine();
+//            for(int j=0;j<n;j++){
+//                matrix[i][j] = str.charAt(j)-'0';
+//            }
+//        }
+//        
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n;j++){
+//                if(!visit[i][j] && matrix[i][j]==1){
+//                    count = 0;
+//                    queue.add(new Matrix(i,j));
+//                    bfs(i,j);
+//                    list.add(count);
+//                }
+//            }
+//        }
+//        Collections.sort(list);
+//        
+//        System.out.println(list.size());
+//        for(int n:list){
+//            System.out.println(n);
+//        }
+//
+//    }
+//    static class Matrix{
+//        int x, y;
+//        Matrix(int x, int y){
+//            this.x = x;
+//            this.y = y;
+//        }
+//    }
+//    static void bfs(int x, int y){
+//        visit[x][y] = true;
+//        count++;
+//        
+//        while(!queue.isEmpty()){
+//            Matrix mx = queue.poll();
+//            for(int i=0;i<4;i++){
+//                int X = mx.x+x_move[i];
+//                int Y = mx.y+y_move[i];
+//                if(X>=0&&Y>=0 && X<n&&Y<n){
+//                    if(matrix[X][Y]==1 && !visit[X][Y]){
+//                        visit[X][Y] = true;
+//                        queue.add(new Matrix(X,Y));
+//                        count++;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
