101d62ab51f75ac8c6cef53cc03f68f915eb1401
SeungjunNA <nsjjsng1@gmail.com>
2023-10-25 21:38:12 +0900

    코딩테스트 대비 문제 풀이

--- src/swea_D3/_4일차_거듭제곱.java
+++ src/swea_D3/_4일차_거듭제곱.java
@@ -1,5 +1,27 @@
 package swea_D3;
 
-public class _4老瞒_芭奠力蚌 {
-
+import java.util.*;
+import java.io.*;
+class _4老瞒_芭奠力蚌
+{
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int t=1;t<=10;t++){
+        	int T = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+            
+            int result = recursion(N, R);
+            System.out.printf("#%d %d\n",t, result);
+        }
+	}
+//	Math.pow(N, R)
+    static int recursion(int n, int r){
+    	if(r == 1){
+            return n;
+        }
+        return recursion(n, r-1) * n;
+    }
 }
