ace4067da518aa30fa4d7aa6b88b5e106ae32f6f
SeungjunNA <nsjjsng1@gmail.com>
2024-07-03 20:53:40 +0900

    refactor: [baekjoon] 최대 힙 다시풀기

--- src/baekjoon/_11279.java
+++ src/baekjoon/_11279.java
@@ -5,18 +5,14 @@ public class _11279 {
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        
         StringBuilder sb = new StringBuilder();
-        PriorityQueue<Integer> q = new PriorityQueue<Integer>(Collections.reverseOrder());
-        for(int i=0;i<n;i++){
-            int x = Integer.parseInt(br.readLine());
-            if(x==0){
-                if(q.isEmpty()) sb.append(0).append("\n");
-                else sb.append(q.poll()).append("\n");
-            }else{
-                q.add(x);
-            }
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<n;i++) {
+        	int k = Integer.parseInt(br.readLine());
+        	pq.add(k);
+        	
+        	if(k==0) sb.append(pq.poll()+"\n");
         }
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
