e33f794b1dadef169a402a65b0a0166a40140775
SeungjunNA <nsjjsng1@gmail.com>
2024-07-09 17:57:17 +0900

    feat: [baekjoon] 알고리즘 수업-알고리즘의 수행 시간1

---
+++ src/baekjoon/_24262.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+public class _24262 {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(0);
+	}
+}
+//MenOfPassion(A[], n) {
+//    i = ⌊n / 2⌋;
+//    return A[i]; # 코드1
+//} 코드 1은 어차피 한번만 실행되고 다항실의 최고차 항은 0일것이므로
